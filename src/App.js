6408f571e7cbeea347c45f58118cbd09fcb34a39
tugaimv <voro6eu@gmail.com>
2020-12-17 17:58:47 +0200

    initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component'
 
 function App() {
+  const[monsters, setMonster] = useState([])
+
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(data => data.json())
+    .then(users => setMonster(users))
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardList monsters={monsters} /> 
     </div>
   );
 }
