a7855945c239c3bbf2cc26cc1d5492c642f41097
tugaimv <30296401+tugaimv@users.noreply.github.com>
2020-12-21 08:29:54 +0200

    search-box

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import { CardList } from './components/card-list/card-list.component'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 function App() {
-  const[monsters, setMonster] = useState([])
-
+  const [monsters, setMonster] = useState([]);
+  const [searchInput, changeInput] = useState("");
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(data => data.json())
-    .then(users => setMonster(users))
-  })
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((data) => data.json())
+      .then((users) => setMonster(users));
+  });
+
+  const filteredMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
 
   return (
     <div className="App">
-      <CardList monsters={monsters} /> 
+      <SearchBox
+        placeholder="input monster name"
+        handleChange={(e) => changeInput(e.target.value)}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
